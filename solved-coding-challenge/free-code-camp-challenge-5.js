c6314620de771c987cd8e910dd0cf85d64bc7b5b
LearnerAnuprash <info.anuprash@gmail.com>
2025-10-30 10:50:08 +0545

    feat:Solved freecodecamp challenge 5

--- solved-coding-challenge/free-code-camp-challenge-5.js
+++ solved-coding-challenge/free-code-camp-challenge-5.js
@@ -1,21 +1,40 @@
-// function sort(emails) {
+// Email Sorter
 
-//   return emails;
-// }
+// On October 29, 1971, the first email ever was sent, introducing the username@domain format we still use. Now, there are billions of email addresses.
 
-let emails = ["jehndoe@gmail", "johndoe@example", "aaradhana@gmail"];
-emails.sort();
-let domains = [];
-let usernames = [];
+// In this challenge, you are given a list of email addresses and need to sort them alphabetically by domain name first (the part after the @), and username second (the part before the @).
 
-for (let email of emails) {
-  let index = email.indexOf("@");
-  domains.push(email.slice(index + 1));
-  usernames.push(email.slice(0, index));
+//     Sorting should be case-insensitive.
+//     If more than one email has the same domain, sort them by their username.
+//     Return an array of the sorted addresses.
+//     Returned addresses should retain their original case.
+
+// For example, given ["jill@mail.com", "john@example.com", "jane@example.com"], return ["jane@example.com", "john@example.com", "jill@mail.com"].
+
+// My solution:
+function sort(emails) {
+  // the higher order function sort() takes a function as argument
+  emails.sort((a, b) => {
+    let [userA, domainA] = a.split("@"); // splitting the domain part and the username part of the email
+    let [userB, domainB] = b.split("@"); // user A + domain A  = email a , userB + domainB = email b , and it shifts to right
+
+    domainA = domainA.toLowerCase(); // Since , we need case-insensitive operation
+    domainB = domainB.toLowerCase();
+    userA = userA.toLowerCase();
+    userB = userB.toLowerCase();
+
+    if (domainA < domainB) return -1; // if a<b ( compares alphabetically ),
+    //  a comes before b
+    if (domainA > domainB) return 1; // if a>b , b comes before a
+
+    if (userA < userB) return -1; // same for username and this only runs
+    // when the domain for 2 values is equal
+    if (userA > userB) return 1;
+
+    return 0; // return 0 states that both the two elements have same
+    // domain and username value so keep their order as it is
+  });
+  return emails; // returns the array of sorted strings
 }
 
-domains.sort();
-usernames.sort();
-
-console.log(domains);
-console.log(emails);
+console.log(sort(["johndoe@gmail", "aradhana@example", "mintu@gmail"]));
